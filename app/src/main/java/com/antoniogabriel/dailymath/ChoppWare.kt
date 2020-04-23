e69e03b088d9eabe4cf27a49a036c86ac5385928
AntonioGSC <antoniogabrielagsc@gmail.com>
2020-04-23 15:35:27 -0300

    finishing IPVA converters and ChoppWare

--- app/src/main/java/com/antoniogabriel/dailymath/ChoppWare.kt
+++ app/src/main/java/com/antoniogabriel/dailymath/ChoppWare.kt
@@ -1,12 +1,26 @@
 package com.antoniogabriel.dailymath
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_chopp_ware.*
 
 class ChoppWare : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chopp_ware)
+
+        btnResultadoAlcool.setOnClickListener {
+            val canecas = edtAlcool.text.toString().toDouble()
+            val litros = String.format("%.2f", (canecas * 300)/1000).toDouble()
+
+            txtResultadoAlcool1.text = "$litros litros de chopp"
+        }
+
+        btnHomeAlcool.setOnClickListener {
+            startActivity(Intent(this@ChoppWare, MainActivity::class.java))
+            finish()
+        }
     }
 }
--- app/src/main/java/com/antoniogabriel/dailymath/Ipva.kt
+++ app/src/main/java/com/antoniogabriel/dailymath/Ipva.kt
@@ -1,12 +1,60 @@
 package com.antoniogabriel.dailymath
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.activity_conversor_temperatura.*
+import kotlinx.android.synthetic.main.activity_ipva.*
 
 class Ipva : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ipva)
+
+        val listaEstados = arrayListOf("Selecione o Estado", "AC", "AL", "AM", "AP", "BA", "CE", "DF", "ES", "GO", "MA", "MT", "MS", "MG", "PA", "PB", "PR", "PE", "PI", "RJ", "RN", "RO", "RS", "RR", "SC", "SE", "SP", "TO")
+        val estadosAdapter = ArrayAdapter(
+            this@Ipva,
+            android.R.layout.simple_spinner_dropdown_item,
+            listaEstados
+        )
+        spnIpva.adapter = estadosAdapter
+
+        btnResultadoIpva.setOnClickListener {
+            val escolha = spnIpva.selectedItem.toString()
+            val valorCarro = edtIpva.text.toString().toDouble()
+
+            when(escolha){
+                "SC", "ES", "SE", "PB", "AC", "TO" -> {
+                    val IPVA = valorCarro * 0.02
+                    txtResultadoIpva.text = "R$ $IPVA"
+                }
+                "AL", "PE", "RN", "CE", "PI", "MA", "BA", "PA", "MS", "GO" -> {
+                    val IPVA = valorCarro * 0.025
+                    txtResultadoIpva.text = "R$ $IPVA"
+                }
+                "RS", "SP", "AP", "AM", "RR", "RO", "MT", "DF" -> {
+                    val IPVA = valorCarro * 0.03
+                    txtResultadoIpva.text = "R$ $IPVA"
+                }
+                "PR" -> {
+                    val IPVA = valorCarro * 0.035
+                    txtResultadoIpva.text = "R$ $IPVA"
+                }
+                "RJ", "MG" -> {
+                    val IPVA = valorCarro * 0.04
+                    txtResultadoIpva.text = "R$ $IPVA"
+                }
+                else -> {
+                    txtResultadoIpva.text = "Selecione um Estado"
+                }
+            }
+        }
+
+        btnHomeIpva.setOnClickListener {
+            startActivity(Intent(this@Ipva, MainActivity::class.java))
+            finish()
+        }
     }
 }
