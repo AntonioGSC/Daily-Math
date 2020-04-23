9ae5ccba7785da01d793a7419fad363c3cb4a6d9
AntonioGSC <antoniogabrielagsc@gmail.com>
2020-04-23 02:19:08 -0300

    add distance_converter and temperature_converter

--- app/src/main/java/com/antoniogabriel/dailymath/ConversorKM.kt
+++ app/src/main/java/com/antoniogabriel/dailymath/ConversorKM.kt
@@ -1,12 +1,30 @@
 package com.antoniogabriel.dailymath
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_conversor_k_m.*
 
 class ConversorKM : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_conversor_k_m)
+
+        btnResultadoKm.setOnClickListener {
+            val quilometro = edtKm.text.toString().toFloat()
+            val metros = quilometro * 1000
+            val centimetros = metros * 100
+            val milimetros = centimetros * 10
+
+            txtResultadoKm1.text = "$metros Metros"
+            txtResultadoKm2.text = "$centimetros Centímetros"
+            txtResultadoKm3.text = "$milimetros Milímetros"
+        }
+
+        btnHomeKm.setOnClickListener {
+            startActivity(Intent(this@ConversorKM, MainActivity::class.java))
+            finish()
+        }
     }
 }
--- app/src/main/java/com/antoniogabriel/dailymath/ConversorTemperatura.kt
+++ app/src/main/java/com/antoniogabriel/dailymath/ConversorTemperatura.kt
@@ -1,12 +1,62 @@
 package com.antoniogabriel.dailymath
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_ano_bissexto.*
+import kotlinx.android.synthetic.main.activity_conversor_temperatura.*
 
 class ConversorTemperatura : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_conversor_temperatura)
+
+        val listaTemperatura = arrayListOf("Selecione a escala", "Celsius", "Kelvin", "Fahrenheit")
+        val temperaturaAdapter = ArrayAdapter(
+            this@ConversorTemperatura,
+            android.R.layout.simple_spinner_dropdown_item,
+            listaTemperatura
+        )
+        spnTemp.adapter = temperaturaAdapter
+
+        btnResultadoTemp.setOnClickListener {
+            val escolha = spnTemp.selectedItem.toString()
+            val temperatura = edtTemp.text.toString().toFloat()
+
+            if(escolha == "Celsius"){
+                val tempKelvin = String.format("%.2f", temperatura + 273.15).toDouble()
+                val tempFahrenheit = String.format("%.2f", (temperatura * 1.8) + 32).toDouble()
+
+                txtResultadoTemp1.text = "Kelvin: $tempKelvin K"
+                txtResultadoTemp2.text = "Fahrenheit: $tempFahrenheit °F"
+            }
+            else if(escolha == "Kelvin"){
+                val tempCelsius = String.format("%.2f", temperatura - 273.15).toDouble()
+                val tempFahrenheit = String.format("%.2f", (tempCelsius * 1.8) + 32).toDouble()
+
+                txtResultadoTemp1.text = "Celsius: $tempCelsius °C"
+                txtResultadoTemp2.text = "Fahrenheit: $tempFahrenheit °F"
+            }
+            else if(escolha == "Fahrenheit"){
+                val tempCelsius = String.format("%.2f", (temperatura - 32) / 1.8).toDouble()
+                val tempKelvin = String.format("%.2f", tempCelsius + 273.15).toDouble()
+
+                txtResultadoTemp1.text = "Celsius: $tempCelsius °C"
+                txtResultadoTemp2.text = "Kelvin: $tempKelvin K"
+            }
+            else{
+                Toast.makeText(
+                    this@ConversorTemperatura,
+                    "Campo Escala Obrigatório!!",
+                    Toast.LENGTH_LONG).show()
+            }
+        }
+        btnHomeTemp.setOnClickListener {
+            startActivity(Intent(this@ConversorTemperatura, MainActivity::class.java))
+            finish()
+        }
     }
 }
